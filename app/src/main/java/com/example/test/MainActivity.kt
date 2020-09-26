a1bbfa0521e5031677fbab3c70d5c91551e514de
SangDv <sang.utehy@gmail.com>
2020-09-26 16:56:08 +0700

    remove querr

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -4,13 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.example.test.simplexml.SimpleXmlParser
-import vn.sangdv.simplexml.connection.QueryElement
-import vn.sangdv.simplexml.connection.RequestAsynTask
 import vn.sangdv.simplexml.model.Optional
 
 class MainActivity : AppCompatActivity() {
     private var TAG = "MainActivity"
-    private var requestAsynTask: RequestAsynTask? = null
     var mListOptional = ArrayList<Optional>()
     private val URL = "https://localHost"
     private var data = "<soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">\n" +
@@ -41,30 +38,6 @@ class MainActivity : AppCompatActivity() {
         var entry = SimpleXmlParser.parse(data, SelfOrderKioskService::class.java!!)
         Log.e(TAG,"==>"+entry.toString())
         Log.e(TAG,"==>"+entry?.body?.makeCustSurvery?.returnData?.toString())
-        mListOptional.add(Optional("lat", "12"))
-        mListOptional.add(Optional("long", "10"))
-        mListOptional.add(
-            Optional(
-                "shopCode",
-                "212"
-            )
-        )
-        mListOptional.add(Optional("imageCheckIn", ""))
-        var a = QueryElement().addElement(mListOptional)
-        requestAsynTask = RequestAsynTask(URL,
-            "xml",
-            object : RequestAsynTask.Callback{
-                override fun onSuccessResponse(response: String?) {
-                    Log.e(TAG,"onSuccess: ==>"+response.toString())
-                }
-
-                override fun onError(message: String?) {
-                    Log.e(TAG,"onError: ==>"+message.toString())
-                }
-
-            }
-        )
-        requestAsynTask?.AsyncTaskRequest()?.execute()
     }
 
 }
